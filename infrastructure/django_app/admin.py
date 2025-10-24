c33fb3b4d76977cb1c841d399bbd3935f1f1b191
Matthew <139655695+Matthew-abg@users.noreply.github.com>
2025-10-24 23:31:39 +0300

    feat(admin): register first learning models for data management     - Registered LearningUnit, LearningBlock, and LearningContent in Django Admin

--- infrastructure/django_app/admin.py
+++ infrastructure/django_app/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from .models import LearningBlockModel
+from .models import LearningContentModel, LearningBlockModel, LearningUnitModel
+
+@admin.register(LearningContentModel)
+class LearningContentAdmin(admin.ModelAdmin):
+    list_display = ("id", "data")
 
 @admin.register(LearningBlockModel)
 class LearningBlockAdmin(admin.ModelAdmin):
     list_display = ("id", "title", "xp", "created_at")
+
+@admin.register(LearningUnitModel)
+class LearningUnitAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "type", "xp", "created_at")
